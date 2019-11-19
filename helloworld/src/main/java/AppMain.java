dfbc3d87d19f2bff1eaf545471b265e249b66381
kingreatwill <350840291@qq.com>
2019-11-19 16:48:56 +0800

    q

--- helloworld/src/main/java/AppMain.java
+++ helloworld/src/main/java/AppMain.java
@@ -1,6 +1,6 @@
 public class AppMain {
     public static void main(String[] args){
-        Thread.onSpinWait();
+        //Thread.onSpinWait();
         System.out.println("hello world.");
     }
 }
