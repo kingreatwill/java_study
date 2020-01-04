dceef92f6ad2005900b2f1a711c55c7fa74dc16d
kingreatwill <350840291@qq.com>
2020-01-04 14:04:10 +0800

    flink

--- FlinkDemo/src/main/java/SocketWindowWordCount.java
+++ FlinkDemo/src/main/java/SocketWindowWordCount.java
@@ -41,6 +41,15 @@ public class SocketWindowWordCount {
         // get the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+//        Properties properties = new Properties();
+//        properties.setProperty("bootstrap.servers", "localhost:9092");
+//// only required for Kafka 0.8
+//        properties.setProperty("zookeeper.connect", "localhost:2181");
+//        properties.setProperty("group.id", "test");
+//        DataStream<String> stream = env
+//                .addSource(new FlinkKafkaConsumer010<>("topic", new SimpleStringSchema(), properties));
+
+
         // get input data by connecting to the socket
         DataStream<String> text = env.socketTextStream(hostname, port, "\n");
 
